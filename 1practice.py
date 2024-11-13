ab5bc17d15c9af03f08df2e5030d264103cdf5a6
Egorwhite61 <egorwhite61@gmail.com>
2024-11-14 00:26:12 +0300

    Add files via upload

---
+++ 1practice.py
@@ -0,0 +1,15 @@
+# 1st programm
+print (9**0.5*5)
+# 2nd programm
+print (9.99<99.8 and 1000!=1001)
+# 3rd programm
+print(2*2+2, (2+2)*2, (2+2)*2==2*2+2)
+# 4th programm
+x=float('123.456')*10
+print(x)
+a=x%10
+print (a)
+b=int(a)
+print(b)
+
+
